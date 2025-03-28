177c8325ea173c8914e0b0a87cc2e379a4c4a3f5
ProfeMelola <melola@educa.madrid.org>
2025-03-28 13:26:48 +0100

    Usar Persona en App

--- src/App.java
+++ src/App.java
@@ -1,8 +1,9 @@
+import model.Persona;
 import util.Utilidades;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //System.out.println("Hello, World!");
-        Utilidades.imprimir("Hola Oscar!!!");
+        Persona persona = new Persona("Mari Luz", "Elola");
+        Utilidades.imprimir(persona.toString());
     }
 }

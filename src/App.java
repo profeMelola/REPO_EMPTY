798fcb06c8f02871c0ad8c0621cb5ab526c4f0fb
ProfeMelola <melola@educa.madrid.org>
2025-03-28 13:06:12 +0100

    Usar metodo imprimir de utilidades

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import util.Utilidades;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
+        Utilidades.imprimir("Hello, World!");
     }
 }

d11769f0f056fe6b748337cded80bad56f4611bb
ProfeMelola <melola@educa.madrid.org>
2025-03-28 13:07:12 +0100

    Cambiar mensaje de saludo

--- src/App.java
+++ src/App.java
@@ -3,6 +3,6 @@ import util.Utilidades;
 public class App {
     public static void main(String[] args) throws Exception {
         //System.out.println("Hello, World!");
-        Utilidades.imprimir("Hello, World!");
+        Utilidades.imprimir("Hola Oscar!!!");
     }
 }
